70055fdbc66f3d2aae82cd100cd5298720f7c2cb
AlinaFasolina <alinaskripets92@gmail.com>
2019-09-25 15:50:09 +0300

    finish My-account page. Add Contact Alumno page

---
+++ contact/contact.js
@@ -0,0 +1,78 @@
+import { MDCTopAppBar } from "@material/top-app-bar";
+import { MDCDrawer } from "@material/drawer";
+import { MDCMenu } from '@material/menu';
+
+jQuery(document).ready(() => new myRequests().init());
+
+class myRequests {
+
+    constructor () {
+        this.state = {
+            fixedSideBar: true,
+            drawer: null
+        };
+        this.drawer(this.state);
+    }
+
+    dropDown (context) {
+        console.log(jQuery(context).parent().find('.mdc-menu')[0]);
+        new MDCMenu(jQuery(context).parent().find('.mdc-menu')[0]).open = true;
+    }
+
+    drawer (state) {
+        state.drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
+        const topAppBar = MDCTopAppBar.attachTo(document.getElementById('app-bar'));
+        topAppBar.setScrollTarget(document.getElementById('main-content'));
+        topAppBar.listen('MDCTopAppBar:nav', () => state.drawer.open = !state.drawer.open);
+    }
+
+    changeSideBarType (context, state) {
+        if (context.innerWidth < 1024 && state.fixedSideBar) {
+            console.log('hew1');
+            state.fixedSideBar = false;
+            jQuery('.mdc-drawer').addClass('mdc-drawer--modal');
+        }
+
+        if (context.innerWidth > 1024 && !state.fixedSideBar) {
+            state.drawer.open = false;
+            state.fixedSideBar = true;
+            jQuery('.mdc-drawer').removeClass('mdc-drawer--modal');
+        }
+    }
+
+    initSideBar (context, state) {
+        if (context.innerWidth < 1024) {
+            state.fixedSideBar = false;
+            jQuery('.mdc-drawer').addClass('mdc-drawer--modal');
+        } else {
+            state.fixedSideBar = true;
+            jQuery('.mdc-drawer').removeClass('mdc-drawer--modal');
+        }
+    }
+
+    changeTab (context) {//context = clicked element
+        console.log(context);
+        $('.graph-tab').removeClass("graph-tab-active");
+        $(context).addClass("graph-tab-active");
+		var currentId = $('.graph-tab').index(context);
+        console.log(currentId);
+        $('.one-tab-info').css('display','none');
+        $('.one-tab-info').eq(currentId).css('display','block');
+    }
+
+    init () {
+        const state = this.state;
+        const dropDown = this.dropDown;
+        jQuery('.mdc-data-table__cell-dots').click(function () { dropDown(this) });
+        const changeSideBarType = this.changeSideBarType;
+        jQuery(window).resize(function () { changeSideBarType(this, state) });
+        setTimeout(() => {
+            this.initSideBar(window, state);
+        })
+
+        const changeTab = this.changeTab;
+        jQuery('.graph-tab').click(function () { changeTab(this) })
+    }
+}
+
+
