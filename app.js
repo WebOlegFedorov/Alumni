21aa558e7d73a1b4270a9ac59a57da1e37f4e9d7
AlinaFasolina <alinaskripets92@gmail.com>
2019-09-27 16:56:25 +0300

    add header drawer to index page

--- app.js
+++ app.js
@@ -7,11 +7,60 @@ import { MDCTextField } from '@material/textfield';
 
 
 window.onload = function() {
+
+    var state = {
+        fixedSideBar: true,
+        drawer: null
+    };
     
     if ($('div').hasClass('mdc-chip-set')) {
         const chipSetEl = document.querySelector('.mdc-chip-set');
         const chipSet = new MDCChipSet(chipSetEl);
     }
+
+
+    function drawer (state) {
+        state.drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
+        const topAppBar = MDCTopAppBar.attachTo(document.getElementById('app-bar'));
+        topAppBar.setScrollTarget(document.getElementById('main-content'));
+        topAppBar.listen('MDCTopAppBar:nav', () => state.drawer.open = !state.drawer.open);
+    }
+    drawer(state);
+
+    function openMenu () {
+        const menu = new MDCMenu(document.querySelector('.mdc-menu'));
+        menu.open = !menu.open;
+    }
+
+    jQuery('.open-submenu').click(() => openMenu());
+
+    function changeSideBarType (context, state) {
+        if (context.innerWidth < 1024 && state.fixedSideBar) {
+            console.log('hew1');
+            state.fixedSideBar = false;
+            jQuery('.mdc-drawer').addClass('mdc-drawer--modal');
+        }
+
+        if (context.innerWidth > 1024 && !state.fixedSideBar) {
+            state.drawer.open = false;
+            state.fixedSideBar = true;
+            jQuery('.mdc-drawer').removeClass('mdc-drawer--modal');
+        }
+    }
+
+    jQuery(window).resize(function () { changeSideBarType(this, state) });
+
+    function initSideBar (context, state) {
+        if (context.innerWidth < 1024) {
+            state.fixedSideBar = false;
+            jQuery('.mdc-drawer').addClass('mdc-drawer--modal');
+        } else {
+            state.fixedSideBar = true;
+            jQuery('.mdc-drawer').removeClass('mdc-drawer--modal');
+        }
+    }
+
+    initSideBar(window, state);
 
     /*Dropdown menu*/
     const menu = new MDCMenu(document.querySelector('.mdc-menu'));
