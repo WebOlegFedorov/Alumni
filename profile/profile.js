a53ce8f8cb1d720ffb53628443972614b55108ea
AlinaFasolina <alinaskripets92@gmail.com>
2019-09-27 15:48:17 +0300

    add black top menu to Profile page.Finish mobile version of Profile page

--- profile/profile.js
+++ profile/profile.js
@@ -1,11 +1,53 @@
 import { MDCRipple } from '@material/ripple';
 import { MDCMenu } from '@material/menu';
+import { MDCTopAppBar } from "@material/top-app-bar";
+import { MDCDrawer } from "@material/drawer";
+
 jQuery(document).ready(() => new Profile().init());
 
 class Profile {
 
     constructor () {
+        this.state = {
+            fixedSideBar: true,
+            drawer: null
+        };
+        this.drawer(this.state);
+    }
 
+    drawer (state) {
+        state.drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
+        const topAppBar = MDCTopAppBar.attachTo(document.getElementById('app-bar'));
+        topAppBar.setScrollTarget(document.getElementById('main-content'));
+        topAppBar.listen('MDCTopAppBar:nav', () => state.drawer.open = !state.drawer.open);
+    }
+
+    dropDown (context) {
+        new MDCMenu(jQuery(context).parent().find('.mdc-menu')[0]).open = true;
+    }
+
+    changeSideBarType (context, state) {
+        if (context.innerWidth < 1024 && state.fixedSideBar) {
+            console.log('hew1');
+            state.fixedSideBar = false;
+            jQuery('.mdc-drawer').addClass('mdc-drawer--modal');
+        }
+
+        if (context.innerWidth > 1024 && !state.fixedSideBar) {
+            state.drawer.open = false;
+            state.fixedSideBar = true;
+            jQuery('.mdc-drawer').removeClass('mdc-drawer--modal');
+        }
+    }
+
+    initSideBar (context, state) {
+        if (context.innerWidth < 1024) {
+            state.fixedSideBar = false;
+            jQuery('.mdc-drawer').addClass('mdc-drawer--modal');
+        } else {
+            state.fixedSideBar = true;
+            jQuery('.mdc-drawer').removeClass('mdc-drawer--modal');
+        }
     }
 
     buttonAnimation () {
@@ -20,6 +62,11 @@ class Profile {
     init () {
         this.buttonAnimation();
         jQuery('.open-submenu').click(() => this.openMenu());
+        const state = this.state;
+        const dropDown = this.dropDown;
+        const changeSideBarType = this.changeSideBarType;
+        jQuery(window).resize(function () { changeSideBarType(this, state) });
+        this.initSideBar(window, state);
     }
 }
 
