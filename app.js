4f50903a4c2ef784cae331a5ec4dc9a9e349cdc2
Alina Skripets <alina.skripets@maven.one>
2019-11-27 15:30:45 +0200

    added adaptive filter toggle bar

--- app.js
+++ app.js
@@ -1,107 +1,130 @@
-import { MDCRipple } from '@material/ripple';
-import { MDCChipSet } from '@material/chips';
+import { MDCRipple } from "@material/ripple";
+import { MDCChipSet } from "@material/chips";
 import { MDCDrawer } from "@material/drawer";
-import { MDCTopAppBar } from '@material/top-app-bar';
-import { MDCMenu } from '@material/menu';
-import { MDCTextField } from '@material/textfield';
-
+import { MDCTopAppBar } from "@material/top-app-bar";
+import { MDCMenu } from "@material/menu";
+import { MDCTextField } from "@material/textfield";
 
 window.onload = function() {
-    var state = {
-        fixedSideBar: true,
-        drawer: null
-    };
+  var state = {
+    fixedSideBar: true,
+    drawer: null
+  };
 
-    
-    if ($('div').hasClass('mdc-chip-set')) {
-        const chipSetEl = document.querySelector('.mdc-chip-set');
-        const chipSet = new MDCChipSet(chipSetEl);
-    }
+  if ($("div").hasClass("mdc-chip-set")) {
+    const chipSetEl = document.querySelector(".mdc-chip-set");
+    const chipSet = new MDCChipSet(chipSetEl);
+  }
 
+  function drawer(state) {
+    state.drawer = MDCDrawer.attachTo(document.querySelector(".mdc-drawer"));
+    const topAppBar = MDCTopAppBar.attachTo(document.getElementById("app-bar"));
+    topAppBar.setScrollTarget(document.getElementById("main-content"));
+    topAppBar.listen(
+      "MDCTopAppBar:nav",
+      () => (state.drawer.open = !state.drawer.open)
+    );
+  }
+  drawer(state);
 
-    function drawer (state) {
-        state.drawer = MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
-        const topAppBar = MDCTopAppBar.attachTo(document.getElementById('app-bar'));
-        topAppBar.setScrollTarget(document.getElementById('main-content'));
-        topAppBar.listen('MDCTopAppBar:nav', () => state.drawer.open = !state.drawer.open);
-    }
-    drawer(state);
+  function openMenu() {
+    const menu = new MDCMenu(document.querySelector(".mdc-menu"));
+    menu.open = !menu.open;
+  }
 
-    function openMenu () {
-        const menu = new MDCMenu(document.querySelector('.mdc-menu'));
-        menu.open = !menu.open;
-    }
+  function openFilters() {
+    jQuery(".find-alumni--filters-block").slideToggle();
+    jQuery(".overlay-bg").toggle();
+  }
+  function countFilterPosition() {
+    const searchHeight = $(".find-alumni--search").css("height");
+    $(".find-alumni--filters-block").css("top", searchHeight);
+  }
 
-    jQuery('.open-submenu').click(() => openMenu());
+  countFilterPosition();
 
-    function changeSideBarType (context, state) {
-        
+  jQuery(".open-submenu").click(() => openMenu());
+
+  jQuery(".find-alumni--search-filters").click(() => openFilters());
+
+  function changeSideBarType(context, state) {
     console.log(state);
-        if (context.innerWidth < 1024 && state.fixedSideBar) {
-            console.log('hew1');
-            state.fixedSideBar = false;
-            jQuery('.mdc-drawer').addClass('mdc-drawer--modal');
-        }
-
-        if (context.innerWidth > 1024 && !state.fixedSideBar) {
-            state.drawer.open = false;
-            state.fixedSideBar = true;
-            jQuery('.mdc-drawer').removeClass('mdc-drawer--modal');
-        }
+    if (context.innerWidth < 1024 && state.fixedSideBar) {
+      console.log("hew1");
+      state.fixedSideBar = false;
+      jQuery(".mdc-drawer").addClass("mdc-drawer--modal");
     }
 
-    jQuery(window).resize(function () { changeSideBarType(this, state) });
-
-    function initSideBar (context, state) {
-        if (context.innerWidth < 1024) {
-            state.fixedSideBar = false;
-            jQuery('.mdc-drawer').addClass('mdc-drawer--modal');
-        } else {
-            state.fixedSideBar = true;
-            jQuery('.mdc-drawer').removeClass('mdc-drawer--modal');
-        }
+    if (context.innerWidth > 1024 && !state.fixedSideBar) {
+      state.drawer.open = false;
+      state.fixedSideBar = true;
+      jQuery(".mdc-drawer").removeClass("mdc-drawer--modal");
     }
+  }
 
-    initSideBar(window, state);
+  jQuery(window).resize(function() {
+    changeSideBarType(this, state);
+    countFilterPosition();
+  });
 
-    /*Dropdown menu*/
-    const menu = new MDCMenu(document.querySelector('.mdc-menu'));
-    $('#header-menu-button').click(function() {
-        $('#header-dropdown-menu').toggleClass('mdc-menu-surface--open');
-    })
-
-    if($('div').hasClass('mdc-button')) { /* Check if current page contains mdc-button  - then initialize Ripple effect*/
-        const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));//button
+  function initSideBar(context, state) {
+    if (context.innerWidth < 1024) {
+      state.fixedSideBar = false;
+      jQuery(".mdc-drawer").addClass("mdc-drawer--modal");
+    } else {
+      state.fixedSideBar = true;
+      jQuery(".mdc-drawer").removeClass("mdc-drawer--modal");
     }
-    
-    if($('div').hasClass('mdc-text-field')) { /* Check if current page contains inputs*/
-        const textField1 = new MDCTextField(document.querySelector('.mdc-text-field1'));
-        const textField2 = new MDCTextField(document.querySelector('.mdc-text-field2'));
-        const textField3 = new MDCTextField(document.querySelector('.mdc-text-field3'));
-        const textField4 = new MDCTextField(document.querySelector('.mdc-text-field4'));
-    }
-    
+  }
 
-    /*Check the number of notifications in the header of the page
+  initSideBar(window, state);
+
+  /*Dropdown menu*/
+  const menu = new MDCMenu(document.querySelector(".mdc-menu"));
+  $("#header-menu-button").click(function() {
+    $("#header-dropdown-menu").toggleClass("mdc-menu-surface--open");
+  });
+
+  if ($("div").hasClass("mdc-button")) {
+    /* Check if current page contains mdc-button  - then initialize Ripple effect*/
+    const buttonRipple = new MDCRipple(document.querySelector(".mdc-button")); //button
+  }
+
+  if ($("div").hasClass("mdc-text-field")) {
+    /* Check if current page contains inputs*/
+    const textField1 = new MDCTextField(
+      document.querySelector(".mdc-text-field1")
+    );
+    const textField2 = new MDCTextField(
+      document.querySelector(".mdc-text-field2")
+    );
+    const textField3 = new MDCTextField(
+      document.querySelector(".mdc-text-field3")
+    );
+    const textField4 = new MDCTextField(
+      document.querySelector(".mdc-text-field4")
+    );
+  }
+
+  /*Check the number of notifications in the header of the page
     (If notifications number is less then 10, or more then 10).
-    According to it - change "left" css property of number(inside heart icon)*/ 
-    function checkNotifNumber() {
-        /*Check if current page header contains div with notification number(inside heart)*/
-        if($('p').hasClass('header-notifications--number') ){
-            let notifNumberBlock = document.getElementById("header-notifications--number");
-            let notifNumberText = document.getElementById("header-notifications--number").innerText;
-            if (notifNumberText < 10 ) {
-                notifNumberBlock.style.right = "12px";
-            }
-            else {
-                notifNumberBlock.style.right = "8px";
-            }
-        }
-        
+    According to it - change "left" css property of number(inside heart icon)*/
+
+  function checkNotifNumber() {
+    /*Check if current page header contains div with notification number(inside heart)*/
+    if ($("p").hasClass("header-notifications--number")) {
+      let notifNumberBlock = document.getElementById(
+        "header-notifications--number"
+      );
+      let notifNumberText = document.getElementById(
+        "header-notifications--number"
+      ).innerText;
+      if (notifNumberText < 10) {
+        notifNumberBlock.style.right = "12px";
+      } else {
+        notifNumberBlock.style.right = "8px";
+      }
     }
-    setInterval(checkNotifNumber, 500);
-}
-
-
-
-	
+  }
+  setInterval(checkNotifNumber, 500);
+};
